2cc6f178edd55cf88194987632f5d733765898c4
xsqxsy <1047436112@qq.com>
2019-01-15 02:03:17 +0800

    complete basic functions

--- widget.cpp
+++ widget.cpp
@@ -1,14 +1,144 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QDebug>
+#define cout qDebug() << "[" << __LINE__ << "]"
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    setWindowTitle("xml reader");
+    resize(800, 600);
+
+    QStringList header;
+    header << "entry" << "pages";
+    ui->treeWidget->setHeaderLabels(header);
+
+    QHeaderView* view = ui->treeWidget->header();
+    view->setSectionResizeMode(QHeaderView::ResizeToContents);
+
+
+    readFile("../QXmlStreamReader/sample.xml");
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+bool Widget::readFile(const QString &fileName)
+{
+    bool ret = true;
+
+    QFile file(fileName);
+    bool isOk = file.open(QIODevice::Text | QIODevice::ReadOnly);
+    if(isOk)
+    {
+        reader.setDevice(&file);
+        while(!reader.atEnd())
+        {
+            if(reader.isStartElement())
+            {
+                if("bookindex" == reader.name())
+                {
+                    readBookindexElement();
+                }
+                else
+                {
+                    reader.raiseError("xml document's root element is not bookindex!");
+                }
+                break;
+            }
+            else
+            {
+                reader.readNext();
+            }
+        }
+        file.close();
+    }
+    else
+    {
+        cout << "failed to open file!";
+        ret = false;
+    }
+
+    return ret;
+}
+
+void Widget::readBookindexElement()
+{
+    reader.readNext();
+    while(!reader.atEnd())
+    {
+        if(reader.isEndElement())
+        {
+            break;
+        }
+
+        if(reader.isStartElement())
+        {
+            if("entry" == reader.name())
+            {
+                readEntryElement(ui->treeWidget->invisibleRootItem());
+            }
+            else
+            {
+                skipUnknownElement();
+            }
+        }
+        reader.readNext();
+    }
+
+}
+
+void Widget::readEntryElement(QTreeWidgetItem *parent)
+{
+    QTreeWidgetItem* item = new QTreeWidgetItem(parent);
+    item->setText(0, reader.attributes().value("term").toString());
+
+    reader.readNext();
+    while(!reader.atEnd())
+    {
+        if(reader.isEndElement())
+        {
+            break;
+        }
+        if(reader.isStartElement())
+        {
+            if("entry" == reader.name())
+            {
+                readEntryElement(item);
+            }
+            else if("page" == reader.name())
+            {
+                readPageElement(item);
+            }
+            else
+            {
+                skipUnknownElement();
+            }
+        }
+
+        reader.readNext();
+
+    }
+}
+
+void Widget::readPageElement(QTreeWidgetItem *parent)
+{
+    QString page = reader.readElementText();
+
+    QString allPages = parent->text(1);
+    if (!allPages.isEmpty())
+    {
+        allPages += ", ";
+    }
+    allPages += page;
+    parent->setText(1, allPages);
+}
+
+void Widget::skipUnknownElement()
+{
+
 }
--- widget.h
+++ widget.h
@@ -2,6 +2,9 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QFile>
+#include <QXmlStreamReader>
+#include <QTreeWidgetItem>
 
 namespace Ui {
 class Widget;
@@ -15,8 +18,18 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
+    bool readFile(const QString& fileName);
+
+private:
+    void readBookindexElement();
+    void readEntryElement(QTreeWidgetItem* parent);
+    void readPageElement(QTreeWidgetItem* parent);
+    void skipUnknownElement();
+
 private:
     Ui::Widget *ui;
+    QXmlStreamReader reader;
+
 };
 
 #endif // WIDGET_H
